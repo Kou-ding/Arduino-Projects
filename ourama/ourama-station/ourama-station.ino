ebf3561e01f761973d2d298a41e72d1af1c57454
Kou-ding <fotispapadak@gmail.com>
2025-05-15 14:41:10 +0300

    station code for ourama

--- ourama/ourama-station/ourama-station.ino
+++ ourama/ourama-station/ourama-station.ino
@@ -1,61 +1,38 @@
-//Arduino Code - RC522 Read RFID Tag UID
- 
+/*
+* Arduino Wireless Communication Tutorial
+*       Example 1 - Receiver Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+
 #include <SPI.h>
-#include <MFRC522.h>
 #include <nRF24L01.h>
 #include <RF24.h>
- 
-#define SS_PIN 10
-#define RST_PIN 7
-RF24 radio(7, 8); // CE, CSN
+
+RF24 radio(9, 10); // CE, CSN
 
 const byte address[6] = "00001";
- 
-MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
- 
-MFRC522::MIFARE_Key key; 
- 
-void setup() { 
-  Serial.begin(9600);
-  SPI.begin(); // Init SPI bus
-  rfid.PCD_Init(); // Init RC522 
 
+void setup() {
+  Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setChannel(108);
+  radio.setRetries(5,15);
   radio.startListening();
 }
- 
+
 void loop() {
- 
-  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-  if ( ! rfid.PICC_IsNewCardPresent())
-    return;
- 
-  // Verify if the NUID has been readed
-  if ( ! rfid.PICC_ReadCardSerial())
-    return;
- 
-  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
- 
-  Serial.print(F("RFID Tag UID:"));
-  printHex(rfid.uid.uidByte, rfid.uid.size);
-  Serial.println("");
- 
-  rfid.PICC_HaltA(); // Halt PICC
-    ///////////////////////////////////////////////////////
-  // NRF24L01
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
-  }
-}
- 
-//Routine to dump a byte array as hex values to Serial. 
-void printHex(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], HEX);
+
+  char receivedUID[9]={0};
+  if (radio.available())  {
+    radio.read(&receivedUID, sizeof(receivedUID));
+    // Serial.print("Received UID:");
+    Serial.println(receivedUID);
+    radio.flush_rx();
   }
 }
